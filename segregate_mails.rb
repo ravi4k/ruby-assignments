dd6efb23b1f245a1f52a2bfbcff5100c1dc039c6
Ravi K <ravi4kumawat@gmail.com>
2021-12-10 05:26:50 +0530

    Segregate Emails into Separate CSVs Based on Their State

--- segregate_mails.rb
+++ segregate_mails.rb
@@ -3,15 +3,21 @@
 require 'faker'
 require 'csv'
 
-def generate_random_data
-  possible_states = %w[delivered bounced sent opened failed]
-  rows = 5000
-  file_path = 'random_mails.csv'
+class CSVInfo
+  ROWS = 5000
+  POSSIBLE_STATES = %w[delivered bounced sent opened failed].freeze
+  FILE_PATH = 'random_mails.csv'
+  OUTPUT_FOLDER = 'output'
+  OUTPUT_PATHS = POSSIBLE_STATES.map do |state|
+    "#{OUTPUT_FOLDER}/result_#{state}.csv"
+  end
+end
 
-  CSV.open(file_path, 'w') do |csv|
-    rows.times do
+def generate_random_data
+  CSV.open(CSVInfo::FILE_PATH, 'w') do |csv|
+    CSVInfo::ROWS.times do
       mail = Faker::Internet.email
-      state = possible_states.sample
+      state = CSVInfo::POSSIBLE_STATES.sample
       csv << [mail, state]
     end
   end
@@ -20,3 +26,23 @@ end
 def refresh_list
   generate_random_data
 end
+
+def segregate_mails
+  refresh_list unless File.exist?(CSVInfo::FILE_PATH)
+  Dir.mkdir(CSVInfo::OUTPUT_FOLDER) unless Dir.exist?(CSVInfo::OUTPUT_FOLDER)
+
+  csv_file = CSV.open(CSVInfo::FILE_PATH, 'r')
+  result_files = CSVInfo::OUTPUT_PATHS.map do |path|
+    CSV.open(path, 'w')
+  end
+
+  csv_file.each do |line|
+    state = line[1]
+
+    CSVInfo::POSSIBLE_STATES.each_index do |idx|
+      result_files[idx] << line if state == CSVInfo::POSSIBLE_STATES[idx]
+    end
+  end
+end
+
+segregate_mails
